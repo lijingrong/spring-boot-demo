0194fe139732723bf6a6217e36031055650aec39
lijingrong <jrliwiscom@gmail.com>
2018-03-13 18:02:37 +0800

    例子改成模块化结构

---
+++ spring-boot-demo-quickstart/src/main/java/com/example/springbootdemo/SpringBootDemoQuickstartApplication.java
@@ -0,0 +1,12 @@
+package com.example.springbootdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDemoQuickstartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDemoQuickstartApplication.class, args);
+	}
+}
