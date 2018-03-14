02250afdbb9cefeb303d1bee8a4e04b9c87115c6
lijingrong <jrliwiscom@gmail.com>
2018-03-14 21:20:24 +0800

    添加数据库连接模块

---
+++ spring-boot-demo-h2/src/main/java/com/example/springbootdemo/SpringBootDemoH2Application.java
@@ -0,0 +1,12 @@
+package com.example.springbootdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDemoH2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDemoH2Application.class, args);
+	}
+}
---
+++ spring-boot-demo-h2/src/main/resources/data-mysql.sql
@@ -0,0 +1,2 @@
+USE test;
+INSERT INTO t_user (user_id,nickname,telephone,email) VALUES ('1234567','jony','15951076438','695214586@qq.com');
